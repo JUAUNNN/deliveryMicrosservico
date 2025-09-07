672ed1ce0490aaa480ee1fbd5d851cbc97f7242b
JUAUNNN <joaovitor061jv@gmail.com>
2025-09-07 19:47:03 -0300

    feat: Adicinando atributos.

--- Microservices/Delivery-Tracking/src/main/java/com/delivery/delivery_tracking/domain/model/ContactPoint.java
+++ Microservices/Delivery-Tracking/src/main/java/com/delivery/delivery_tracking/domain/model/ContactPoint.java
@@ -1,8 +1,12 @@
 package com.delivery.delivery_tracking.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
+@Builder
 public class ContactPoint {
     private String zipCode;
     private String street;
--- Microservices/Delivery-Tracking/src/main/java/com/delivery/delivery_tracking/domain/model/Delivery.java
+++ Microservices/Delivery-Tracking/src/main/java/com/delivery/delivery_tracking/domain/model/Delivery.java
@@ -1,6 +1,8 @@
 package com.delivery.delivery_tracking.domain.model;
 
+import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
@@ -8,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Delivery {
 
@@ -32,5 +35,16 @@ public class Delivery {
     private ContactPoint recipient;
 
     private List<Item> intens = new ArrayList<>();
+
+    public static Delivery draft() {
+        Delivery delivery = new Delivery();
+        delivery.id = UUID.randomUUID();
+        delivery.status = DeliveryStatus.DRAFT;
+        delivery.totalItens = 0;
+        delivery.totalCost = BigDecimal.ZERO;
+        delivery.courierPayout = BigDecimal.ZERO;
+        delivery.distanceFee = BigDecimal.ZERO;
+     return delivery;
+    }
 
 }
