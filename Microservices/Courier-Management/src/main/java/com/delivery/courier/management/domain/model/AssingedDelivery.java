01b46e2b5175a22d667c565cb0b36a80ee227a53
JUAUNNN <joaovitor061jv@gmail.com>
2025-09-23 23:32:54 -0300

    feat: Adicionando novos pacotes, classes e atributos.

---
+++ Microservices/Courier-Management/src/main/java/com/delivery/courier/management/domain/model/AssingedDelivery.java
@@ -0,0 +1,4 @@
+package com.delivery.courier.management.domain.model;
+
+public class AssingedDelivery {
+}
---
+++ Microservices/Courier-Management/src/main/java/com/delivery/courier/management/domain/model/Courier.java
@@ -0,0 +1,25 @@
+package com.delivery.courier.management.domain.model;
+
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@Setter(AccessLevel.PRIVATE)
+public class Courier {
+
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    @Setter(AccessLevel.PUBLIC)
+    private String name;
+
+    @Setter(AccessLevel.PUBLIC)
+    private String phone;
+
+    private Integer fulfilledDeliveriesQuantity;
+
+
+}
